06da635e48aff28104b60968a2a23f4c7804d750
wasabina67 <wasabina67@gmail.com>
2025-04-18 21:08:10 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,3 +3,15 @@ const app = express();
 const port = 3000;
 
 app.set('port', port);
+
+app.use(express.static(__dirname + '/docs'));
+
+app.use((req, res, next) => {
+  res.status(404).send('404 Not Found');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+}).on('error', (err) => {
+  console.error('Server error:', err);
+});
