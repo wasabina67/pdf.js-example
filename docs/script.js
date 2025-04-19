9462568cc92e44f654b5a21034ccdfa5effc540b
wasabina67 <wasabina67@gmail.com>
2025-04-19 17:00:21 +0900

    Update docs/index.html, docs/script.js

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,23 @@
+let pdfDocument = null;
+
 function loadPDF(path) {
-  console.log(path);
+  pdfjsLib.GlobalWorkerOptions.workerSrc = 'pdfjs-5.1.91-dist/build/pdf.worker.mjs';
+  pdfjsLib.GlobalWorkerOptions.cMapUrl = 'pdfjs-5.1.91-dist/web/cmaps/';
+  pdfjsLib.GlobalWorkerOptions.cMapPacked = true;
+
+  const canvas = document.getElementById('canvas-pdf');
+  const ctx = canvas.getContext('2d');
+
+  pdfjsLib.getDocument({
+    url: path,
+    cMapUrl: pdfjsLib.GlobalWorkerOptions.cMapUrl,
+    cMapPacked: pdfjsLib.GlobalWorkerOptions.cMapPacked,
+    useWorkerFetch: true
+  }).promise.then((pdf) => {
+    pdfDocument = pdf;
+  }).catch((err) => {
+    console.error(err);
+  });
 }
 
 function previous() {}
